81f0d4d672d542b01e66823563400093340acac0
mengguiyouziyi <775618369@qq.com>
2017-10-24 18:19:34 +0800

    mingluji_liuzhou

--- jianjie/jianjie/pipelines.py
+++ jianjie/jianjie/pipelines.py
@@ -11,7 +11,7 @@ from jianjie.items import Huangye88KunmingItem, Huangye88LiuzhouItem, ShunqiLiuz
 
 class MysqlPipeline(object):
 	def __init__(self):
-		self.conn = pymysql.connect(host='etl2.innotree.org', port=3308, user='spider', password='spider', db='spider',
+		self.conn = pymysql.connect(host='etl1.innotree.org', port=3308, user='spider', password='spider', db='spider',
 		                            charset='utf8', cursorclass=pymysql.cursors.DictCursor)
 		self.cursor = self.conn.cursor()
 
