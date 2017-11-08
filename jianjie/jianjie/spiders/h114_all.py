e5f16d7853e9f40fc46d78b1cd4bb82fc799f9e8
mengguiyouziyi <775618369@qq.com>
2017-11-08 14:58:09 +0800

    mingluji_liuzhou

--- jianjie/jianjie/spiders/h114_all.py
+++ jianjie/jianjie/spiders/h114_all.py
@@ -3,12 +3,14 @@ import os
 import sys
 from os.path import dirname
 
+ffather_path = dirname(dirname(dirname(os.path.abspath(dirname(__file__)))))
 father_path = dirname(dirname(os.path.abspath(dirname(__file__))))
 base_path = dirname(dirname(os.path.abspath(dirname(__file__))))
 path = dirname(os.path.abspath(dirname(__file__)))
 sys.path.append(path)
 sys.path.append(base_path)
 sys.path.append(father_path)
+sys.path.append(ffather_path)
 import scrapy
 import time
 from jianjie.jianjie.utils.bloomfilter import rc
