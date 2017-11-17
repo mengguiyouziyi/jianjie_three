d83101c4927abc85f9b6d8de92b7e992666f0fe7
mengguiyouziyi <775618369@qq.com>
2017-11-18 02:09:35 +0800

    mingluji_liuzhou

--- jianjie/jianjie/spiders/ca800_shenyang.py
+++ jianjie/jianjie/spiders/ca800_shenyang.py
@@ -8,20 +8,20 @@ from jianjie.items import Ca800Item
 
 class TouzishijianSpider(scrapy.Spider):
 	name = 'ca800_shenyang'
-	custom_settings = {
-		'DEFAULT_REQUEST_HEADERS': {
-			'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-			'accept-encoding': "gzip, deflate",
-			'accept-language': "zh-CN,zh;q=0.8",
-			'cache-control': "no-cache",
-			# 'connection': "keep-alive",
-			# 'cookie': "ASP.NET_SessionId=kh5iqkvjtb4vjthzf40t54x1; cookiesmark=2017111717345000003; Hm_lvt_57a0c48ef04a2d3a7e7dec8e459b2bb4=1510911322; Hm_lpvt_57a0c48ef04a2d3a7e7dec8e459b2bb4=1510911380; sid=2017111717351400003; vid=2017111717351400004; _ga=GA1.2.332251248.1510911440; _gid=GA1.2.926593169.1510911440",
-			'host': "www.ca800.com",
-			'upgrade-insecure-requests': "1",
-			# 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-			# 'postman-token': "05eca74f-a731-dd48-9e85-fba7ae401bc6"
-		}
-	}
+	# custom_settings = {
+	# 	'DEFAULT_REQUEST_HEADERS': {
+	# 		'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+	# 		'accept-encoding': "gzip, deflate",
+	# 		'accept-language': "zh-CN,zh;q=0.8",
+	# 		'cache-control': "no-cache",
+	# 		# 'connection': "keep-alive",
+	# 		# 'cookie': "ASP.NET_SessionId=kh5iqkvjtb4vjthzf40t54x1; cookiesmark=2017111717345000003; Hm_lvt_57a0c48ef04a2d3a7e7dec8e459b2bb4=1510911322; Hm_lpvt_57a0c48ef04a2d3a7e7dec8e459b2bb4=1510911380; sid=2017111717351400003; vid=2017111717351400004; _ga=GA1.2.332251248.1510911440; _gid=GA1.2.926593169.1510911440",
+	# 		'host': "www.ca800.com",
+	# 		'upgrade-insecure-requests': "1",
+	# 		# 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+	# 		# 'postman-token': "05eca74f-a731-dd48-9e85-fba7ae401bc6"
+	# 	}
+	# }
 
 	def start_requests(self):
 		start_urls = ["http://www.ca800.com/company/l_0_0_0_0_2_00_{}.html".format(i) for i in range(1, 9611)]
@@ -54,9 +54,12 @@ class TouzishijianSpider(scrapy.Spider):
 			loc_str = cat_tag.xpath('./text()').extract()
 			loc_str = ''.join([s.strip().replace('\r', '').replace('\t', '') for s in loc_str if s]) if loc_str else ''
 			loc = re.search(r'所在地：(.*)', loc_str).group(1)
-			lia = re.search(r'(.*)-(.*)', loc).groups()
+			lia = re.search(r'(.*)-(.*)', loc)
+			lia = lia.groups() if lia else []
 			sheng = lia[0] if len(lia) > 1 else ''
-			shi = lia[1] if len(lia) > 1 else lia[0]
+			shi = ''
+			if lia:
+				shi = lia[1] if len(lia) > 1 else lia[0]
 
 			intro = li_tag.xpath('./p/text()').extract_first()
 
