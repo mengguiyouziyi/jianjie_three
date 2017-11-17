8cdf45315874765b3cb6140afc903dcefcd06230
mengguiyouziyi <775618369@qq.com>
2017-11-18 02:17:00 +0800

    mingluji_liuzhou

--- jianjie/jianjie/spiders/jiqiren.py
+++ jianjie/jianjie/spiders/jiqiren.py
@@ -7,20 +7,20 @@ from jianjie.items import JiqirenItem
 
 class TouzishijianSpider(scrapy.Spider):
 	name = 'jiqiren'
-	custom_settings = {
-		'DEFAULT_REQUEST_HEADERS': {
-			'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-			'accept-encoding': "gzip, deflate",
-			'accept-language': "zh-CN,zh;q=0.8",
-			'cache-control': "no-cache",
-			# 'connection': "keep-alive",
-			# 'cookie': "UM_distinctid=15fc95c2872d78-0fdfee884d9db7-31637e01-13c680-15fc95c28748a8; CNZZDATA3130222=cnzz_eid%3D346745979-1510908079-%26ntime%3D1510924313; Hm_lvt_99d3e8dc9c4fb1796f922e4fc84251b1=1510911782; Hm_lpvt_99d3e8dc9c4fb1796f922e4fc84251b1=1510928806; AJSTAT_ok_pages=9; AJSTAT_ok_times=2; __tins__5221700=%7B%22sid%22%3A1510928732944%2C%22vd%22%3A9%2C%22expires%22%3A1510930606401%7D; __51cke__=; __51laig__=10",
-			'host': "www.robot-china.com",
-			'upgrade-insecure-requests': "1",
-			# 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
-			# 'postman-token': "455d07fb-baa0-faf6-92c9-95ea6a2d771c"
-		}
-	}
+	# custom_settings = {
+	# 	'DEFAULT_REQUEST_HEADERS': {
+	# 		'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+	# 		'accept-encoding': "gzip, deflate",
+	# 		'accept-language': "zh-CN,zh;q=0.8",
+	# 		'cache-control': "no-cache",
+	# 		# 'connection': "keep-alive",
+	# 		# 'cookie': "UM_distinctid=15fc95c2872d78-0fdfee884d9db7-31637e01-13c680-15fc95c28748a8; CNZZDATA3130222=cnzz_eid%3D346745979-1510908079-%26ntime%3D1510924313; Hm_lvt_99d3e8dc9c4fb1796f922e4fc84251b1=1510911782; Hm_lpvt_99d3e8dc9c4fb1796f922e4fc84251b1=1510928806; AJSTAT_ok_pages=9; AJSTAT_ok_times=2; __tins__5221700=%7B%22sid%22%3A1510928732944%2C%22vd%22%3A9%2C%22expires%22%3A1510930606401%7D; __51cke__=; __51laig__=10",
+	# 		'host': "www.robot-china.com",
+	# 		'upgrade-insecure-requests': "1",
+	# 		# 'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36",
+	# 		# 'postman-token': "455d07fb-baa0-faf6-92c9-95ea6a2d771c"
+	# 	}
+	# }
 
 	def start_requests(self):
 		start_url = "http://www.robot-china.com/company/"
